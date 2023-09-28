4f8ddaf1dd0987805e114f5c9b61698302438a2b
Zoltan Polgar <pozo@gmx.com>
2023-09-28 14:00:43 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "asd"
-
-if
+echo $1                           # Unquoted variables
+find . -name *.ogg                # Unquoted find/grep patterns
+rm "~/my file.txt"                # Quoted tilde expansion
+v='--verbose="true"'; cmd $v      # Literal quotes in variables
+echo 'Don't forget to restart!'   # Singlequote closed by apostrophe
+echo 'Don\'t try this at home'    # Attempting to escape ' in ''
+echo 'Path is $PATH'              # Variables in single quotes
+trap "echo Took ${SECONDS}s" 0    # Prematurely expanded trap
+unset var[i]                      # Array index treated as glob
